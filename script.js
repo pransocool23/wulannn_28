d7939451d19451883e31fc1da6111a87843cd98b
pransocool23 <sri.wulan16959@sma.belajar.id>
2025-12-15 12:58:38 +0700

    Add beli function to handle purchase notifications

---
+++ script.js
@@ -0,0 +1,6 @@
+function beli() {
+    alert("Jam tangan berhasil ditambahkan ke keranjang!");
+
+    document.getElementById("notif").innerText =
+        "✔ Terima kasih! Pesanan Anda sedang diproses.";
+}
